7f5e1f7b671345be1dbe4b50db8d565f23e441d8
pythonshiva <python.shiva@gmail.com>
2023-04-11 15:36:53 +0530

    Variables and local values

--- main.tf
+++ main.tf
@@ -59,3 +59,50 @@ output "bucket_info" {
 resource "aws_s3_bucket" "bucket2" {
   bucket = "${data.aws_caller_identity.current.account_id}-bucket2"
 }
+
+# Dependency
+# Resources can depend on one another. Terraform will ensure that all the dependencies are met
+# before creating a resource. Dependancies can be implicit or exlicit.
+
+resource "aws_s3_bucket" "bucket3" {
+  bucket = "${data.aws_caller_identity.current.account_id}-bucket3"
+  tags = {
+    # Implicit dependency
+    "dependency" = aws_s3_bucket.bucket2.arn
+  }
+}
+resource "aws_s3_bucket" "bucket4" {
+  bucket = "${data.aws_caller_identity.current.account_id}-bucket4"
+
+  depends_on = [
+    aws_s3_bucket.bucket3
+  ]
+}
+
+# Variables
+# Can be specified on the commnd line with -var bucket_name my-bucket or 
+# in files: terraform.tfvars or *.auto.tfvars
+# or in environment variables: TF_VAR_Buckett_name
+
+variable "bucket_name" {
+
+
+  # `default` is an optional default value, If `default` is ommited
+  # then a value must be supplied.
+  # default = "my-bucket"
+}
+
+resource "aws_s3_bucket" "Bucket5" {
+  bucket = var.bucket_name
+}
+
+# Local values
+# Local values allow you to assign a name to an expression. Locals can make your code more readable
+locals {
+  aws_account = "${data.aws_caller_identity.current.account_id}-${lower(data.aws_caller_identity.current.user_id)}"
+}
+
+resource "aws_s3_bucket" "Bucket6" {
+  bucket = "${local.aws_account}-bucket6"
+}
+
